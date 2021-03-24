e8792fec7e66aa12579bb6f88efa91171960c444
Luiz Fernando Verissimo <luizfverissimo@gmail.com>
2021-03-24 14:26:42 -0300

    projectPage mobile DONE

--- components/FooterMenu.js
+++ components/FooterMenu.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 function FooterMenu() {
   return (
-    <nav className="w-full h-auto flex justify-center sm:justify-start fixed bottom-0 mb-0 bg-black-theme pt-4 sm:mb-20 sm:max-w-screen-xl sm:bg-transparent sm:pt-0 sm:px-4 xl:px-0 ">
+    <nav className="w-full h-auto flex justify-center sm:justify-start fixed bottom-0 mb-0 bg-black-theme pt-4 sm:mb-10 sm:max-w-screen-xl sm:bg-transparent sm:pt-0 sm:px-4 xl:px-0 ">
       <Link href="/projects">
         <a className="chromatic-box px-6 py-3 mr-2 sm:mr-8 bg-white-theme font-pop font-bold text-black-theme text-lg sm:text-2xl transition-all transform hover:text-pink-theme hover:-translate-y-1">
           Projects
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,5 +1,7 @@
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+import Carousel from 'nuka-carousel';
 
 import Background from '../../components/Background';
 import FooterMenu from '../../components/FooterMenu';
@@ -23,6 +25,11 @@ const importProjects = async () => {
 };
 
 const Projects = ({ projectsList }) => {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    if (window.innerWidth <= 650) setIsMobile(true);
+  }, []);
+
   return (
     <>
       <Head>
@@ -60,53 +67,127 @@ const Projects = ({ projectsList }) => {
       <main className='w-full min-h-full flex flex-col items-center'>
         <Background />
         <Navbar />
-        <div className='w-full max-w-screen-xl flex justify-between items-end'>
-          <h1 className='chromatic font-pop font-bold text-white-theme text-8xl'>
+        <div className='w-full max-w-screen-xl flex justify-between items-end px-4 xl:px-0 '>
+          <h1 className='chromatic font-pop font-bold text-white-theme text-6xl sm:text-8xl'>
             PROJECTS
           </h1>
         </div>
 
-        <section className='w-full max-w-screen-xl flex flex-auto mt-10 overflow-x-scroll'>
-          {projectsList.map((project, index) => {
-            return (
-              <article
-                key={index}
-                style={{ width: 256 }}
-                className='w-52 mr-28 flex flex-col flex-grow-0 flex-shrink-0 items-start justify-between relative mb-12'
-              >
-                <div className='w-full flex flex-col'>
-                  <h3 className='w-full font-pop font-light text-pink-theme text-xl'>
-                    {project.attributes.type}
-                  </h3>
-                  <h2 className='w-full mt-2 font-pop font-bold text-white-theme text-3xl'>
-                    {project.attributes.title}
-                  </h2>
-                </div>
-
-                <div className='w-full flex flex-col'>
-                  <Link
-                    href='/projects/project/[slug]'
-                    as={`/projects/project/${project.slug}`}
+        <section className='w-full max-w-screen-xl h-full sm:h-auto flex flex-auto mt-5 lg:mt-10'>
+          {!isMobile && (
+            <Carousel
+              defaultControlsConfig={{
+                nextButtonStyle: {
+                  fontFamily: 'Poppins',
+                  fontWeight: 'bold',
+                  borderRadius: '40px 0 0 40px',
+                  fontSize: '24px',
+                  outline: 0
+                },
+                nextButtonText: '>',
+                prevButtonStyle: {
+                  fontFamily: 'Poppins',
+                  fontWeight: 'bold',
+                  borderRadius: '0 40px 40px 0',
+                  fontSize: '24px',
+                  outline: 0
+                },
+                prevButtonText: '<',
+                pagingDotsStyle: {
+                  display: 'none'
+                }
+              }}
+              wrapAround
+              slidesToShow={4}
+              transitionMode={'scroll3d'}
+              cellSpacing={100}
+              enableKeyboardControls
+              slideWidth='400px'
+            >
+              {projectsList.map((project, index) => {
+                return (
+                  <article
+                    key={index}
+                    className='w-full bg-black-theme h-auto p-4 sm:p-8 border rounded-xl border-white-theme flex flex-col items-start justify-between relative shadow-2xl'
                   >
-                    <a>
-                      <img
-                        className='my-6'
-                        src={project.attributes.coverImg}
-                        alt={project.attributes.title}
-                      />
-                    </a>
-                  </Link>
-                  <p className='w-full font-pop text-gray-theme text-xl'>
-                    {project.attributes.desc}
-                  </p>
-                  <div>
-                    <ViewButton slug={project.slug} />
-                    <CodeButton href={project.attributes.codeLink} />
-                  </div>
-                </div>
-              </article>
-            );
-          })}
+                    <div className='w-full flex flex-col'>
+                      <h3 className='w-full font-pop font-light text-pink-theme text-lg sm:text-xl'>
+                        {project.attributes.type}
+                      </h3>
+                      <h2 className='w-full mt-2 font-pop font-bold text-white-theme text-2xl sm:text-3xl'>
+                        {project.attributes.title}
+                      </h2>
+                    </div>
+
+                    <div className='w-full flex flex-col'>
+                      <Link
+                        href='/projects/project/[slug]'
+                        as={`/projects/project/${project.slug}`}
+                      >
+                        <a>
+                          <img
+                            className='my-6'
+                            src={project.attributes.coverImg}
+                            alt={project.attributes.title}
+                          />
+                        </a>
+                      </Link>
+                      <p className='w-full font-pop text-gray-theme text-lg sm:text-xl'>
+                        {project.attributes.desc}
+                      </p>
+                      <div>
+                        <ViewButton slug={project.slug} />
+                        <CodeButton href={project.attributes.codeLink} />
+                      </div>
+                    </div>
+                  </article>
+                );
+              })}
+            </Carousel>
+          )}
+          {isMobile && (
+            <div className="w-full h-auto flex flex-col mt-5 mb-20 px-5">
+              {projectsList.map((project, index) => {
+                return (
+                  <article
+                    key={index}
+                    className='w-full bg-black-theme h-auto p-4 my-5 sm:p-8 border rounded-xl border-white-theme flex flex-col items-start justify-between relative shadow-2xl'
+                  >
+                    <div className='w-full flex flex-col'>
+                      <h3 className='w-full font-pop font-light text-pink-theme text-lg sm:text-xl'>
+                        {project.attributes.type}
+                      </h3>
+                      <h2 className='w-full mt-2 font-pop font-bold text-white-theme text-2xl sm:text-3xl'>
+                        {project.attributes.title}
+                      </h2>
+                    </div>
+
+                    <div className='w-full flex flex-col'>
+                      <Link
+                        href='/projects/project/[slug]'
+                        as={`/projects/project/${project.slug}`}
+                      >
+                        <a>
+                          <img
+                            className='my-6'
+                            src={project.attributes.coverImg}
+                            alt={project.attributes.title}
+                          />
+                        </a>
+                      </Link>
+                      <p className='w-full font-pop text-gray-theme text-lg sm:text-xl'>
+                        {project.attributes.desc}
+                      </p>
+                      <div>
+                        <ViewButton slug={project.slug} />
+                        <CodeButton href={project.attributes.codeLink} />
+                      </div>
+                    </div>
+                  </article>
+                );
+              })}
+            </div>
+          )}
         </section>
 
         <FooterMenu />
