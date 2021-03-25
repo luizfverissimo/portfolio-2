82f163e7f4d0badc6674032f7bca8659004ad460
Luiz Fernando Verissimo <luizfverissimo@gmail.com>
2021-03-25 18:23:20 -0300

    favicon ADDED

--- pages/about.js
+++ pages/about.js
@@ -15,7 +15,27 @@ function About() {
     <>
       <Head>
         <title>About - LF Verissimo | Front-end Developer</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff'></meta>
         <meta
           name='description'
           content='About - LF Verissimo personal portfolio, front-end developer, solving problems through code.'
--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,37 @@ import { motion } from 'framer-motion';
 import Background from '../components/Background';
 import FooterMenu from '../components/FooterMenu';
 import Navbar from '../components/Navbar';
-import {animationChildVariants, animationParentVariants} from '../utils/animations'
+import {
+  animationChildVariants,
+  animationParentVariants
+} from '../utils/animations';
 
 function Home() {
   return (
     <>
       <Head>
         <title>LF Verissimo | Front-end Developer</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff'></meta>
         <meta
           name='description'
           content='LF Verissimo personal portfolio, front-end developer, solving problems through code.'
